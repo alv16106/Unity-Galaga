e8e1532f1c32ed840d4b60053ef18ea9e8cf440c
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2017-03-19 19:51:48 -0600

    Proyecto completo
    Galgaga en Unity

---
+++ Assets/Scripts/DestruirChoque.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirChoque : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Frontera")
+		{
+			return;
+		}
+
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		
+		Instantiate(explosion, other.transform.position, other.transform.rotation);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject enemigo;
+	public Vector3 spawnValues;
+	public int cuentaEnemigos;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnEnemigos());
+	}
+	
+	
+	IEnumerator SpawnEnemigos () {
+		yield return new WaitForSeconds (startWait);
+		while (true){	
+			for (int i =0; i<cuentaEnemigos;i++){
+				Vector3 spawnPos = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Instantiate(enemigo, spawnPos, Quaternion.identity);
+				yield return new WaitForSeconds (spawnWait);	
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+}
